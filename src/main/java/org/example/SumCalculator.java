71fc6b6245a7435de3607993c0638da362af0ffb
Roman Cherkhavskyi <romko-chera@ukr.net>
2023-03-05 13:22:25 +0200

    2nd commit

--- src/main/java/org/example/SumCalculator.java
+++ src/main/java/org/example/SumCalculator.java
@@ -1,2 +1,20 @@
-package org.example;public class SumCalculator {
+package org.example;
+
+public class SumCalculator {
+
+    public int sum(int n) throws IllegalArgumentException{
+        int result = 0;
+
+
+        for (int i = 0; i <= n; i++) {
+            result += i;
+        }
+
+        if (result == 0 && n == 0) {
+            throw new IllegalArgumentException();
+        }
+        else {
+            return result;
+        }
+    }
 }
--- src/test/java/SumCalculatorTests.java
+++ src/test/java/SumCalculatorTests.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class SumCalculatorTests {
+import org.example.SumCalculator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class SumCalculatorTests {
+
+    private SumCalculator calc;
+
+    @BeforeEach
+    public void beforeEach(){
+        calc = new SumCalculator();
+    }
+
+    @Test
+    public void testSumCalculatorCorrectWithOne(){
+        int actual = calc.sum(1);
+        int expected = 1;
+        Assertions.assertEquals(expected, actual);
+//        System.out.println("testSumCalculatorCorrectWithOne is correct");
+    }
+
+    @Test
+    public void testSumCalculatorCorrectWithThree(){
+        int actual = calc.sum(3);
+        int expected = 6;
+        Assertions.assertEquals(expected, actual);
+//        System.out.println("testSumCalculatorCorrectWithThree is correct");
+
+    }
+
+    @Test
+    public void testSumCalculatorCorrectWithZero(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calc.sum(0));
+//        System.out.println("testSumCalculatorCorrectWithZero is correct");
+
+    }
 }
